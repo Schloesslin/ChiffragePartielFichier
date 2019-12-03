b85607a46898b9e63db184dac6670690a768d754
Schloesslin <schloesslin.lucas@gmail.com>
2019-12-03 02:02:47 +0100

    mise en place architeture mvc

---
+++ src/controler/Controler.java
@@ -0,0 +1,18 @@
+package controler;
+
+import model.AbstractModel;
+
+public class Controler extends AbstractControler {
+
+	public Controler(AbstractModel model) {
+		super(model);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void control() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/model/Model.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Model extends AbstractModel {
+
+	@Override
+	public String readFile(String file, int start, int stop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPath(String path) {
+		// TODO Auto-generated method stub
+		this.path=path;
+		
+	}
+
+	@Override
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return this.path;
+	}
+
+	
+
+}
